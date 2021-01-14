4eb7ab2c01ad9ee5e676f92c552af9c85d1dd88a
LinXia <linyianxia@hotmail.com>
2021-01-14 01:12:16 -0500

    add: mobile-responsiveness

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "../style/Banner.css";
 
 const Banner = (props) => {
+  const [toggle, setToggle] = useState(true);
   return (
     <div className="banner-container">
-      <div className="banner">
+      <div
+        className={`banner ${!toggle ? "hide" : ""}`}
+        onClick={() => setToggle(false)}>
         <h2>You've nominated 5 films!</h2>
       </div>
     </div>
